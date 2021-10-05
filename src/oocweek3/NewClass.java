aa3978934fdfa076f66df0067fc17a9e616bd3c5
Caroline de Sá Teixeira <caroline.desat@gmail.com>
2021-10-05 08:27:05 +0100

    V2
    Second version for my new class.

---
+++ src/oocweek3/NewClass.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oocweek3;
+
+/**
+ *
+ * @author carol
+ */
+public class NewClass {
+    
+    public void NewClass() {
+        
+        System.out.println("This is my new class");
+        
+    }
+    
+}
